46b630658767c3082cee8f56b9c1383bbdf30f5e
bangdpc <bangdanh105@gmail.com>
2024-01-20 11:37:16 +0700

    learning unit test doing test title

--- src/components/molecules/title/Title.test.tsx
+++ src/components/molecules/title/Title.test.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Title from './Title';
-const mockProps = {
+import Title, { propsTitle } from './Title'; // Assuming PropsTitle is the interface for props in your Title component
+
+const mockProps: propsTitle = {
     title: "Contact",
-    textPosition:"text-left",
+    textPosition: "text-left",
 };
-describe("render Title Components",()=> {
+
+describe("render Title Components", () => {
     it("should render Title and Position of Title", () => {
-        const {getByText,getByTestId} = render(<Title {...mockProps}/>)
+        const { getByText, getByTestId } = render(<Title {...mockProps} />);
         const titleElement = getByText(mockProps.title);
         const containerElement = getByTestId('title-container');
 
         expect(titleElement).toBeInTheDocument();
         expect(containerElement).toHaveClass(mockProps.textPosition);
     });
-})
+});
